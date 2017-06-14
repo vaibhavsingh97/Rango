95060e0b7f99b3a3f9578589473dc2c2de634d04
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-06-14 23:56:52 +0530

    url error fix + template added

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    # Constructing a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the
+    # template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+    # Returning a rendered response to send to the client.
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
