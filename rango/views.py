2ebbcf4bde4cde1655637cd6764190343373c090
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-06-17 00:02:54 +0530

    saving work

--- rango/views.py
+++ rango/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# Importing category model
+from rango.models import Category
 # Create your views here.
 
 
 def index(request):
-    # Constructing a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the
-    # template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
-    # Returning a rendered response to send to the client.
-    return render(request, 'rango/index.html', context=context_dict)
-
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    # return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
-    return render(request, 'rango/about.html', context={'About':'About page'})
+    # return HttpResponse("Rango says here is the about page.<br/><a
+    # href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html', context={'About': 'About page'})
